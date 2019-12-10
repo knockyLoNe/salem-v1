15606fe80ea4676af72c4fa9298128e06ccf54f0
Nikita <knocky4u@gmail.com>
2019-12-10 15:25:40 +0600

    final

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const imagesArray = ["1", "2", "3", "logo_1", "info"];
+const imagesArray = ["1", "2", "3", "4"];
 
 const img = document.querySelector("img");
+const info = document.querySelector('.info');
+const last = imagesArray[imagesArray.length - 1]
 let i = 0;
 imagesArray.forEach(image => {
   setTimeout(() => {
     img.src = `img/${image}.png`;
-    img.dataset.id = image; // data-id=1,2,3,logo_1
+    img.dataset.id = image;
+    if (img.dataset.id === last) {
+      fadeIn(info)
+    }
   }, 2000 * i);
   ++i;
 });
+
+function fadeIn(el) {
+  el.style.opacity = 0;
+  var tick = function () {
+    el.style.opacity = +el.style.opacity + 0.01;
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+    }
+  };
+  tick();
+}
