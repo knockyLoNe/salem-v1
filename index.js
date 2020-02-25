88fec6e0823cd4307893253f4b72cb9408d355df
Nikita <knocky4u@gmail.com>
2020-02-25 14:18:51 +0600

    obnova

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const imagesArray = ["1", "2", "3", "4"];
+const imagesArray = ['1', '2', '3', '4'];
 
-const img = document.querySelector("img");
+const img = document.querySelector('img');
 const info = document.querySelector('.info');
-const last = imagesArray[imagesArray.length - 1]
+const last = imagesArray[imagesArray.length - 1];
 let i = 0;
 imagesArray.forEach(image => {
   setTimeout(() => {
     img.src = `img/${image}.png`;
     img.dataset.id = image;
     if (img.dataset.id === last) {
-      fadeIn(info)
+      fadeIn(info);
     }
-  }, 1000 * i);
+  }, 900 * i);
   ++i;
 });
 
 function fadeIn(el) {
   el.style.opacity = 0;
-  var tick = function () {
+  var tick = function() {
     el.style.opacity = +el.style.opacity + 0.01;
     if (+el.style.opacity < 1) {
-      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) ||
+        setTimeout(tick, 16);
     }
   };
   tick();
